6f7b5395eb3652530afe024a5f23c40a3b3707d8
anubhavsingh16 <anubhavssingh177@gmail.com>
2021-01-25 11:09:42 +0530

    converter calculation complete

--- ConverterAppi/ConverterAppi/ContentView.swift
+++ ConverterAppi/ConverterAppi/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var input = 1
+    @State private var input = 0
     @State private var output = 1
     @State private var amount = ""
+    
+    var rsult: Float{
+        
+        var Amount: Float = 0
+        let user = Float(amount) ?? 0
+        switch inputType[input] {
+        case "Celsius":
+            if outType[output] == "Celsius"{
+                Amount = user
+            }
+            else if outType[output] == "Fahrenheit"{
+                Amount = (user * 1.8) + 32
+            }
+            else if outType[output] == "kelvin"{
+                Amount = user + 273.15
+            }
+            break
+        
+        case "Fahrenheit":
+        if outType[output] == "Celsius"{
+            Amount = (user - 32)/1.8
+        }
+        else if outType[output] == "Fahrenheit"{
+            Amount = user
+        }
+        else if outType[output] == "kelvin"{
+            Amount = (user + 459.67) * 5/9
+        }
+        break
+            
+        case "kelvin":
+        if outType[output] == "Celsius"{
+            Amount = user - 273.15
+        }
+        else if outType[output] == "Fahrenheit"{
+            Amount = (user * 9/5) - 459.67
+        }
+        else if outType[output] == "kelvin"{
+            Amount = user
+        }
+        break
+            
+            
+        default:
+            break
+        }
+        return Amount
+    }
     
     
     var inputType = ["Celsius","Fahrenheit","kelvin"]
@@ -46,7 +94,7 @@ struct ContentView: View {
                 }
                 
                 Section{
-                    Text("\(amount) \(inputType[input]) in \(outType[output]) is : ")
+                    Text("\(amount) \(inputType[input]) in \(outType[output]) is : \(rsult, specifier: "%.2f") ")
                 }
             }
             .navigationBarTitle("Converter", displayMode: .inline)
