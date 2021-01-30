96c88d2a712606e192b768f8023613c267c634ce
anubhavsingh16 <anubhavssingh177@gmail.com>
2021-01-30 14:08:42 +0530

    modification of outout with conditional modifier

--- splitter/splitter/ContentView.swift
+++ splitter/splitter/ContentView.swift
@@ -1,54 +1,75 @@
 import SwiftUI
-
-  struct ContentView: View {
-     @State private var checkAmount = ""
-     @State private var tipPercentage = 1
-     @State private var numberOfPeople = 2
-     var tipPercentages = ["5","10","15","20"]
-     var body: some View {
-         NavigationView{
-             Form{
-                 Section{
-                     TextField("Enter amount to pay:", text: $checkAmount)
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var tipPercentage = 1
+    @State private var numberOfPeople = 2
+    var tipPercentages = ["0","5","10","15","20"]
+    
+    var totalPerPerson: Double {
+        let amount = Double(checkAmount) ?? 0
+        let tipP = Double(tipPercentages[tipPercentage])!
+        let people = Double(numberOfPeople + 1)
+        
+        let tipA = (tipP * amount) / 100
+        print("The tipA is \(tipA)")
+        
+        let total = Double((amount + tipA) / people)
+        return total
+    }
+    var condition: Bool{
+        let check = Double(tipPercentages[tipPercentage])!
+        var option = false
+        if check == 0{
+            option = true
+        }
+        return option
+    }
+    
+    var body: some View {
+        
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter amount to pay:", text: $checkAmount)
                          .keyboardType(.decimalPad)
                      Text("Cost to pay is: \(checkAmount)")
                  }
-                 Section(header: Text("Select % Tip")){
-                     Picker("Percent of Tip", selection: $tipPercentage){
-                         ForEach(0..<tipPercentages.count){
-                             Text(self.tipPercentages[$0])
-                         }
-                     }
-                     .pickerStyle(SegmentedPickerStyle())
-                 }
+                Section(header: Text("Select Tip %")){
+                       Picker("Percent of Tip", selection: $tipPercentage){
+                           ForEach(0..<tipPercentages.count){
+                               Text(self.tipPercentages[$0])
+                           }
+                       }
+                       .pickerStyle(SegmentedPickerStyle())
+                    
+              
+                }
+                
                  Section{
                      VStack{
-                         Picker("Number of people:", selection: $numberOfPeople){
-                             ForEach(0..<10){
-                                 Text("\($0) people")
-                             }
-                         }
- //                        Text("Number of pople is : \(numberOfPeople)")
+                        Picker("Number of people:", selection: $numberOfPeople){
+                                                   ForEach(1..<10){
+                                                        Text("\($0) people")
+                                                    }
+                                                }
                      }
-                 }
+                     }
+                Section(header: Text("Per person Pay:")){
+                                     Text("$\(totalPerPerson, specifier: "%.2f")")
+                                        .foregroundColor(condition ? .red : .blue)
+                                 }
+                             }
+                             .navigationTitle("Splitter")
+                            .navigationBarTitleDisplayMode(.inline)
+                        }
+                    }
 
-                  let tipPay = Int(checkAmount) ?? 0
+    
+                }
+                struct ContentView_Previews: PreviewProvider {
+                    static var previews: some View {
+                        ContentView()
+                    }
+                }
 
-                  let total = (tipPay + tipPercentage) / numberOfPeople
-
-                  Section{
-                     Text("1 Person pay:  \(total)")
-                 }
-             }
-             .navigationTitle("Splitter")
-             .navigationBarTitleDisplayMode(.inline)
-         }
-     }
- }
-
-  struct ContentView_Previews: PreviewProvider {
-     static var previews: some View {
-         ContentView()
-     }
- }
 
